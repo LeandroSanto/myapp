968bde03d6ea7c4d6e93a67295a79801a8a23048
andros.the.maker <andros.the.maker@gmail.com>
2025-04-16 17:39:21 +0000

    segundo dia de desenvlvimento tela home

--- lib/ui/_core/app_colors.dart
+++ lib/ui/_core/app_colors.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 abstract class AppColors {
-  static const Color backgroundColor = Color(0xFF202123);
   static const Color mainColor = Color(0xFFffa559);
+  static const Color buttonPressedColor = Color(0xFFFF6000);
+  static const Color textColor = Color(0xFFFFE6C7);
+  static const Color backgroundColor = Color(0xFF202123);
+  static const Color lightBackgroundColor = Color(0xFF343541);
+  static const Color textCardsColor = Color(0xFFDBD7DF);
 }
